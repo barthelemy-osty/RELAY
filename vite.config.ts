3d53ab38a95729a5dc0da35641ff388b291fc406
barthelemy-osty <u65620731@gmail.com>
2026-06-24 01:08:31 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,17 +3,4 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      'libsodium-wrappers': 'libsodium-wrappers/dist/modules/libsodium-wrappers.js',
-    },
-  },
-  optimizeDeps: {
-    include: ['libsodium-wrappers'],
-  },
-  build: {
-    commonjsOptions: {
-      transformMixedEsModules: true,
-    },
-  },
 })
